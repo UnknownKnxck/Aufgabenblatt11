116f4cec052cfad0b4bb18ed9dba21d13d41f3eb
OmaCrocs <mirko.ungerank@snets.it>
2023-03-15 20:06:19 +0100

    volle fertig

--- src/b/List.java
+++ src/b/List.java
@@ -1,4 +1,115 @@
 package b;
 
-public class List {
+import java.util.NoSuchElementException;
+
+public class List<T> {
+    private Object[] array;
+    private Node<T> head;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public List() {
+        array = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public void add(T element) {
+        if (size == array.length) {
+            increaseArrayCapacity();
+        }
+        array[size++] = element;
+    }
+
+    public void add(T element, int index) {
+        try {
+            if (index < 0 || index > size) {
+                throw new IndexOutOfBoundsException();
+            }
+            if (size == array.length) {
+                increaseArrayCapacity();
+            }
+            for (int i = size; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+            size++;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds");
+        }
+    }
+
+    public void set(T element, int index) {
+        try {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            array[index] = element;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds");
+        }
+    }
+
+    public T remove() {
+        try {
+            if (size == 0) {
+                throw new NoSuchElementException();
+            }
+            T element = (T) array[--size];
+            array[size] = null;
+            return element;
+        } catch (NoSuchElementException e) {
+            System.out.println("No such element");
+            return null;
+        }
+    }
+
+    public T remove(int index) {
+        try {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            T element = (T) array[index];
+            for (int i = index; i < size - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[--size] = null;
+            return element;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds");
+            return null;
+        }
+    }
+
+    public T get(int index) {
+        try {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            return (T) array[index];
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds");
+            return null;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void increaseArrayCapacity() {
+        int newCapacity = array.length * 2;
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+    }
+
+    private static class Node<T> {
+        T element;
+        Node<T> next;
+
+        Node(T element) {
+            this.element = element;
+            next = null;
+        }
+    }
 }
--- src/b/Main.java
+++ src/b/Main.java
@@ -1,9 +1,45 @@
 package b;
-
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        List<String> list = new List<>();
 
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        System.out.println("List after adding elements: " + listToString(list));
+
+        list.add("e", 2);
+        System.out.println("List after adding element at index: " + listToString(list));
+
+        list.set("f", 3);
+        System.out.println("List after setting element at index: " + listToString(list));
+
+        String removedElement = list.remove();
+        System.out.println("Removed element: " + removedElement);
+        System.out.println("List after removing last element: " + listToString(list));
+
+        removedElement = list.remove(1);
+        System.out.println("Removed element: " + removedElement);
+        System.out.println("List after removing element at index: " + listToString(list));
+
+        String element = list.get(2);
+        System.out.println("Element at index 2: " + element);
+
+        System.out.println("Size of list: " + list.size());
+    }
+
+    private static String listToString(List<?> list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i < list.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
